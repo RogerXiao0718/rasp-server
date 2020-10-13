a0cf1de57db8a3b572122ee968c2c2bd13ce17bb
Roger <deo2468977@gmail.com>
2020-10-13 17:29:03 +0800

    Add GET method for / path

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, './rasp-client/build')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './rasp-client/build/index.html'));
+});
 
 app.listen(4000, () => {
     console.log("You are listening port 4000");
